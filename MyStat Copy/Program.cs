72d8714cf1f7160f95a52082d081392ea9825144
AmiraslanSumgait <emiraslaneliyev45@gmail.com>
2021-08-05 19:14:04 +0400

    Test

--- MyStat Copy/Program.cs
+++ MyStat Copy/Program.cs
@@ -14,7 +14,7 @@ namespace MyStat_Copy
         [STAThread]
         static void Main()
         {
-            //Lets go
+            //Lets gooo
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
